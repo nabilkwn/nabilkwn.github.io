05731c680d3c1a7090a193d25ec35d62f6a03813
nabilkwn <nabilkurniawan6908@gmail.com>
2023-11-15 20:01:11 +0700

    Update Portofolio -V2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,54 +1,75 @@
 // Navbar Fixed
 window.onscroll = function () {
-  const header = document.querySelector('header');
+  const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector('#to-top');
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed');
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
-    header.classList.remove('navbar-fixed');
-    toTop.classList.remove('flex');
-    toTop.classList.add('hidden');
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function () {
-  hamburger.classList.toggle('hamburger-active');
-  navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // Klik di luar hamburger
-window.addEventListener('click', function (e) {
+window.addEventListener("click", function (e) {
   if (e.target != hamburger && e.target != navMenu) {
-    hamburger.classList.remove('hamburger-active');
-    navMenu.classList.add('hidden');
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
   }
 });
 
 // Darkmode toggle
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', function () {
+darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
-    html.classList.add('dark');
-    localStorage.theme = 'dark';
+    html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
-    html.classList.remove('dark');
-    localStorage.theme = 'light';
+    html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
 
 // pindahkan posisi toggle sesuai mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
   darkToggle.checked = true;
 } else {
   darkToggle.checked = false;
+}
+
+// Get the modal by id
+var modal = document.getElementById("modal");
+
+// Get the modal image tag
+var modalImg = document.getElementById("modal-img");
+
+// this function is called when a small image is clicked
+function showModal(src) {
+  modal.classList.remove("hidden");
+  modalImg.src = src;
+}
+
+// this function is called when the close button is clicked
+function closeModal() {
+  modal.classList.add("hidden");
 }
